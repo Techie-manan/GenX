fc3de7c14aadd3120329c196747ad4feafe035b1
Manan <aggarwalmanan18@gmail.com>
2025-06-11 12:24:20 +0530

    11/6

--- src/components/CenterBlob.jsx
+++ src/components/CenterBlob.jsx
@@ -5,16 +5,7 @@ export function CenterBlob() {
   return (
     <>
       <div className="absolute left-1/3  z-1">
-        {/* <div className="rot1">
-      <div className="urv1"></div>
-      <div className="urv2"></div>
-    </div>
-    <div className="rot2">
-      <div className="urv3"></div>
-      <div className="urv4"></div>
-    </div> */}
-
-        <div className="rot1">
+               <div className="rot1">
           <img src="./genesis.png" alt="" height={100} className='' />
         </div>
       </div>
