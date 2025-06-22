ca8091aeda59f9fcc54e2bae01d1ea9b8c04b939
Manan <aggarwalmanan18@gmail.com>
2025-06-22 13:27:14 +0530

    22/6

--- src/components/HeroScene.jsx
+++ src/components/HeroScene.jsx
@@ -6,9 +6,9 @@ import { Starfield } from "./Starfield.jsx";
 export default function HeroScene() {
   return (
     <>
-      <Canvas className="z-0"
+      <Canvas className="pointer-events-none"
         camera={{ position: [0, 0, 5], fov: 75 }}
-        style={{ position: "absolute", top: 0, left: 0, zIndex: -1 }}
+        style={{ position: "absolute", top: 0, left: 0, zIndex: -10 }}
       >
         <color attach="background" args={["#000000"]} />
         <ambientLight intensity={0.4} />
