a7f4f9ccf232105ff9de2524d8ad6126f3acceb4
Manan <aggarwalmanan18@gmail.com>
2025-07-07 23:33:47 +0530

    7/7

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -28,21 +28,21 @@ const about = () => {
                     {/* </div> */}
                 </div>
                 <h1 className='text-center text-5xl font-bold p-5 mt-10'>Themes</h1>
-                <div className="centre flex-col md:flex-row gap-5 md:justify-around text-center px-[5vw]">
+                <div className="centre flex-row gap-5 md:justify-around text-center px-[5vw]">
                     <FadeUp>
-                        <div className="card capsule w-[80vw] md:w-[27vw] md:flex-1/4 bg-[url('/images/web3.png')] bg-cover bg-center h-[35vh]">
+                        <div className="card capsule w-[27vw] md:flex-1/4 bg-[url('/images/web3.png')] bg-cover bg-center h-25 md:h-50">
                         </div>
                     </FadeUp>
                     <FadeUp delay={0.2}>
-                        <div className="card centre capsule w-[80vw] md:w-[27vw] md:flex-1/4 bg-[url('/images/fusion.png')] bg-cover bg-center  h-[35vh] flex items-center justify-center">
-                            <h2 className='p-5 text-xl font-semibold text-white text-center'>
+                        <div className="card centre capsule w-[27vw] md:flex-1/4 bg-[url('/images/fusion.png')] bg-cover bg-center  h-25 md:h-50 flex items-center justify-center">
+                            <h2 className='p-5 text-sm md:text-xl font-semibold text-white text-center'>
                                 AI x Web3 <br /> Fusion
                             </h2>
                         </div>
                     </FadeUp>
                     <FadeUp delay={0.4}>
-                        <div className="card centre capsule w-[80vw] md:w-[27vw] md:flex-1/4 bg-[url('/images/ai.png')] bg-cover bg-center h-[35vh] flex justify-center items-center">
-                            <h2 className='py-1 px-2 text-2xl font-semibold text-center backdrop-blur-xs rounded-full'>AI</h2>
+                        <div className="card centre capsule w-[27vw] md:flex-1/4 bg-[url('/images/ai.png')] bg-cover bg-center h-25 md:h-50 flex justify-center items-center">
+                            <h2 className='py-1 px-2 text-lg md:text-2xl font-semibold text-center backdrop-blur-xs rounded-full'>AI</h2>
                         </div>
                     </FadeUp>
                 </div>
