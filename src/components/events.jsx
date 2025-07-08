ecae8ab1a27cc5a93a2efcab189cc5458e59227d
Manan <aggarwalmanan18@gmail.com>
2025-07-08 21:29:58 +0530

    8/7

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -7,7 +7,7 @@ const events = () => {
         <>
             <div id='events' className='z-10 text-white bg pt-10 pb-27'>
                 <h1 className='text-center text-5xl font-bold p-5 '>Timeline</h1>
-                <div className='w-[100vw] h-400 sm:h-300 mt-5 pl-5 flex lg:justify-center lg:pl-0'>
+                <div className='w-[100vw] h-500 sm:h-400 mt-5 pl-5 flex lg:justify-center lg:pl-0'>
                     <img src="./images/eventbg.jpg" alt="" className='absolute opacity-40' />
                     <div className='flex flex-col w-1 relative h-full bg-gray-500 rounded-full z-20'>
                         <div className="circle -top-1">
@@ -22,7 +22,7 @@ const events = () => {
                                 </div>
                             </FadeRight>
                         </div>
-                        <div className="circle top-1/5">
+                        <div className="circle top-1/10">
                             <FadeLeft>
                                 <div className='shadow-lg shadow-red-500 box3 left-7 lg:-left-128'>
 
@@ -35,7 +35,7 @@ const events = () => {
                                 </div>
                             </FadeLeft>
                         </div>
-                        <div className="circle top-2/5">
+                        <div className="circle top-2/10">
                             <FadeRight>
                                 <div className='box2 shadow-lg shadow-fuchsia-500'>
 
@@ -48,7 +48,7 @@ const events = () => {
                                 </div>
                             </FadeRight>
                         </div>
-                        <div className="circle top-3/5">
+                        <div className="circle top-3/10">
                             <FadeLeft>
                                 <div className='shadow-lg shadow-red-500 box3 left-7 lg:-left-128'>
 
@@ -61,7 +61,7 @@ const events = () => {
                                 </div>
                             </FadeLeft>
                         </div>
-                        <div className="circle top-4/5">
+                        <div className="circle top-4/10">
                             <FadeRight>
                                 <div className='box2 shadow-lg shadow-fuchsia-500'>
 
@@ -74,7 +74,7 @@ const events = () => {
                                 </div>
                             </FadeRight>
                         </div>
-                        <div className="circle -bottom-1">
+                        <div className="circle top-5/10">
                             <FadeLeft>
                                 <div className='shadow-lg shadow-red-500 box3 left-7 lg:-left-128'>
 
@@ -82,6 +82,57 @@ const events = () => {
                                         <div className="text-xl font-semibold">Blackhole Break </div>
                                         <div className="text-gray-500 font-medium">04:00 PM</div>
                                         <p className='mt-2 list-none'>Quick energizer, coffee + networking zone.
+                                        </p>
+                                    </div>
+                                </div>
+                            </FadeLeft>
+                        </div>
+                        <div className="circle top-6/10">
+                            <FadeRight>
+                                <div className='shadow-lg shadow-fuchsia-500 box2'>
+                                    <div>
+                                        <div className="text-xl font-semibold">Code Meteor Shower</div>
+                                        <div className="text-gray-500 font-medium">05:00 PM</div>
+                                        <p className='mt-2 list-none'>Side challenge: Build a tiny feature, win goodies.
+                                        </p>
+                                    </div>
+                                </div>
+                            </FadeRight>
+                        </div>
+                        <div className="circle top-7/10">
+                            <FadeLeft>
+                                <div className='shadow-lg shadow-red-500 box3 left-7 lg:-left-128'>
+
+                                    <div>
+                                        <div className="text-xl font-semibold">AI & Web3 Nebula Talk</div>
+                                        <div className="text-gray-500 font-medium">06:30 PM</div>
+                                         <p className='mt-2 list-none'>Expert speaker session on frontier tech.
+                                        </p>
+                                    </div>
+                                </div>
+                            </FadeLeft>
+                        </div>
+                        <div className="circle top-8/10">
+                            <FadeRight>
+                                <div className='box2 shadow-lg shadow-fuchsia-500'>
+
+                                    <div>
+                                        <div className="text-xl font-semibold">Meteor Bites</div>
+                                        <div className="text-gray-500 font-medium">08:00 PM</div>
+                                        <p className='mt-2 list-none'>Dinner is served! Refuel for the long orbit.
+                                        </p>
+                                    </div>
+                                </div>
+                            </FadeRight>
+                        </div>
+                        <div className="circle top-9/10">
+                            <FadeLeft>
+                                <div className='shadow-lg shadow-red-500 box3 left-7 lg:-left-128'>
+
+                                    <div>
+                                        <div className="text-xl font-semibold">Mission Pulse Check</div>
+                                        <div className="text-gray-500 font-medium">09:00 PM</div>
+                                         <p className='mt-2 list-none'>	Midway team check-ins with mentors/judges.
                                         </p>
                                     </div>
                                 </div>
